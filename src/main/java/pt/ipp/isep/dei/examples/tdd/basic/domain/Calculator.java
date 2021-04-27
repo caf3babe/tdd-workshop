df163369608be1a60cd7a2cf6b64ab64435a1c12
Nuno Bettencourt <nmb@isep.ipp.pt>
2021-04-27 13:36:52 +0100

    Initial commit.

---
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/domain/Calculator.java
@@ -0,0 +1,35 @@
+package pt.ipp.isep.dei.examples.tdd.basic.domain;
+
+/**
+ * Calculator class.
+ * This class is very simple in order to demonstrate how to build test cases for Unit Testing.
+ */
+public class Calculator {
+
+    /**
+     * Sums one operand to the other, returning the result.
+     *
+     * @param firstOperand  First operand to sum.
+     * @param secondOperand Second Operand to sum.
+     * @return The sum of firstOperand with secondOperand.
+     */
+    public int sum(int firstOperand, int secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    public int subtract(int firstOperand, int secondOperand) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int divide(int dividend, int divisor) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int multiply(int dividend, int divisor) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int factorial(int number) {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ src/main/java/pt/ipp/isep/dei/examples/tdd/basic/ui/Main.java
@@ -0,0 +1,11 @@
+package pt.ipp.isep.dei.examples.tdd.basic.ui;
+
+import pt.ipp.isep.dei.examples.tdd.basic.domain.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.sum(3, 4));
+    }
+}
